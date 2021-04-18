119d08c3c8a11c635a556217180d267b5003dceb
Jason <jasonbeaverw99@gmail.com>
2021-04-18 12:37:41 -0400

    Added Firebase implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:herd/Login.dart';
-import 'package:herd/screens/login_screen.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:herd/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:herd/blocs/authentication_bloc/authentication_event.dart';
+import 'package:herd/repositories/user_repository.dart';
 
-void main(){
-  runApp(herd());
+main(){
+  runApp(Herd());
 }
 
-class herd extends StatelessWidget {
+class Herd extends StatefulWidget {
+  State<Herd> createState() => _herdState();
+}
+
+class _herdState extends State<Herd>{
+  final UserRepository _userRepository = UserRepository();
+  AuthenticationBloc _authenticationBloc;
+
+  @override
+  void initState(){
+    _authenticationBloc = AuthenticationBloc(userRepository: _userRepository);
+    _authenticationBloc.add(AuthenticationStarted());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'HERD',
-      theme: ThemeData(),
-      home: LoginScreen(),
+    return BlocProvider(
+        create: (BuildContext context) => _authenticationBloc,
+        child: MaterialApp (
+          home: BlocBuilder(
+            bloc: _authenticationBloc,
+            builder: (BuildContext context),
+          ),
+        ),
     );
   }
+}
+
+class AppStarted {
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,8 +5,149 @@ class LoginScreen extends StatelessWidget{
   @override
   Widget build(BuildContext build){
     return Scaffold(
-      body: (
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF550723),
+                    Color(0xFF8E1843),
+                    Color(0xFFFF6060),
+                  ],
+                  stops: [0.1, 0.3, 0.7],
+                )
+            ),
+          ),
 
+          Container(
+            height: double.infinity,
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.symmetric(
+                vertical: 120,
+                horizontal: 40,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Arial',
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  SizedBox(height: 30.0),
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      //Email or phone number
+                      Container(
+                        //Email or phone number information
+                        child: TextField(
+                          //controller: emailController,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                              borderSide: BorderSide(color: Colors.purple, width: 2),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                              borderSide: BorderSide(color: Colors.black38, width: 2.0),
+                            ),
+                            labelText: 'Email',
+                            labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'OpenSans',
+                              fontSize: 15,
+                            ),
+                            prefixIcon: Icon(
+                              Icons.email,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 25.0),
+                      //Password
+                      Container(
+                        child: TextField(
+                          //controller: passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                              borderSide: BorderSide(color: Colors.purpleAccent, width: 2),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                              borderSide: BorderSide(color: Colors.black38, width: 2),
+                            ),
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'OpenSans',
+                              fontSize: 15,
+                            ),
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: 50),
+                      ButtonTheme(
+                        minWidth: 200,
+                        height: 50,
+                        child: RaisedButton(
+                          onPressed: () {
+
+                          },
+                          shape: const StadiumBorder(),
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          child: Text('Sign In'),
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+                      ButtonTheme(
+                        minWidth: 200,
+                        height: 50,
+                        child: RaisedButton(
+                          onPressed: (){
+
+                          },
+                          shape: const StadiumBorder(),
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          child: Text('Sign Up'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
