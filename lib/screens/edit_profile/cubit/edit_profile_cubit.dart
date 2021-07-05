7702ca42091e027c42c2585aedd2300ed5e6ed36
LeaveItToBeaver <jjadood@gmail.com>
2021-07-05 13:51:23 -0400

    Created the User_Model as well as finished various forms of the custom_router.dart

--- lib/screens/edit_profile/cubit/edit_profile_cubit.dart
+++ lib/screens/edit_profile/cubit/edit_profile_cubit.dart
@@ -1,12 +1,11 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/foundation.dart';
 import 'package:herd/models/models.dart';
 import 'package:herd/repositories/repositories.dart';
-import 'package:herd/repositories/storage/storage_repository.dart';
 import 'package:herd/screens/profile/bloc/profile_bloc.dart';
 import 'package:meta/meta.dart';
-import 'dart:io';
 
 part 'edit_profile_state.dart';
 
@@ -15,34 +14,33 @@ class EditProfileCubit extends Cubit<EditProfileState> {
   final StorageRepository _storageRepository;
   final ProfileBloc _profileBloc;
 
-  EditProfileCubit(
-      UserRepository userRepository,
-      StorageRepository storageRepository,
-      ProfileBloc profileBloc,
-      ) :
-        _userRepository = userRepository,
+  EditProfileCubit({
+    @required UserRepository userRepository,
+    @required StorageRepository storageRepository,
+    @required ProfileBloc profileBloc,
+  })  : _userRepository = userRepository,
         _storageRepository = storageRepository,
         _profileBloc = profileBloc,
-        super(EditProfileState.initial()){
-    final user =_profileBloc.state.user;
+        super(EditProfileState.initial()) {
+    final user = _profileBloc.state.user;
     emit(state.copyWith(username: user.username, bio: user.bio));
   }
 
-  void profileImageChanged(File image){
+  void profileImageChanged(File image) {
     emit(
       state.copyWith(profileImage: image, status: EditProfileStatus.initial),
     );
   }
 
-  void usernameChanged(String username){
+  void usernameChanged(String username) {
     emit(
-        state.copyWith(username: username, status: EditProfileStatus.initial),
+      state.copyWith(username: username, status: EditProfileStatus.initial),
     );
   }
 
-  void bioChanged(String bio){
+  void bioChanged(String bio) {
     emit(
-        state.copyWith(bio: bio, status: EditProfileStatus.initial),
+      state.copyWith(bio: bio, status: EditProfileStatus.initial),
     );
   }
 
@@ -50,19 +48,19 @@ class EditProfileCubit extends Cubit<EditProfileState> {
     emit(state.copyWith(status: EditProfileStatus.submitting));
     try {
       final user = _profileBloc.state.user;
-      var profileImageUrl = user.profileImageURL;
 
-      if (state.profileImage != null){
+      var profileImageUrl = user.profileImageURL;
+      if (state.profileImage != null) {
         profileImageUrl = await _storageRepository.uploadProfileImage(
-            url: profileImageUrl,
-            image: state.profileImage,
+          url: profileImageUrl,
+          image: state.profileImage,
         );
       }
 
       final updatedUser = user.copyWith(
         username: state.username,
         bio: state.bio,
-        profileImageURL: profileImageUrl
+        profileImageURL: profileImageUrl,
       );
 
       await _userRepository.updateUser(user: updatedUser);
@@ -72,10 +70,12 @@ class EditProfileCubit extends Cubit<EditProfileState> {
       emit(state.copyWith(status: EditProfileStatus.success));
     } catch (err) {
       emit(
-          state.copyWith(
-              status: EditProfileStatus.error,
-              failure: const Failure(message: 'Sorry, we were unable to update your profile.'),
-          )
+        state.copyWith(
+          status: EditProfileStatus.error,
+          failure: const Failure(
+            message: 'We were unable to update your profile.',
+          ),
+        ),
       );
     }
   }
--- lib/screens/edit_profile/edit_profile_screen.dart
+++ lib/screens/edit_profile/edit_profile_screen.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:herd/repositories/repositories.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:herd/screens/edit_profile/cubit/edit_profile_cubit.dart';
 
 class EditProfileScreen extends StatelessWidget {
   static const String routeName ='/editProfile';
 
-  static Route route(){
+  static Route route() {
     return MaterialPageRoute(
       settings: const RouteSettings(name: routeName),
       builder: (context) => BlocProvider<EditProfileCubit>(
         create: (_) => EditProfileCubit(
-          userRepository: context.read<UserRepository>(),
+            userRepository: context.read<UserRepository>(),
+            storageRepository: context.read<StorageRepository>(),
+            profileBloc: null,
         ),
-        child: Container(),
-      )
+      ),
     );
   }
 
   @override
-  Widget build(BuildContext contxt){
+  Widget build(BuildContext context){
     return Container(
 
     );
