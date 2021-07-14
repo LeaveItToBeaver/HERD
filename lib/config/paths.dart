426e515b7168997e896314c33152a08ca1358c8f
LeaveItToBeaver <jjadood@gmail.com>
2021-07-13 21:59:21 -0400

    Created the post repository and man was it a doozy.

--- lib/config/paths.dart
+++ lib/config/paths.dart
@@ -1,4 +1,9 @@
 class Paths {
   //Top level Collections
   static const String users = 'users';
+  static const String posts = 'posts';
+  static const String comments = 'comments';
+
+  //subcollections
+  static const String postComments = 'postComments';
 }
--- lib/repositories/repositories.dart
+++ lib/repositories/repositories.dart
@@ -6,3 +6,5 @@ export 'user/user_repository.dart';
 export 'user/base_user_repository.dart';
 export 'storage/storage_repository.dart';
 export 'storage/base_storage_repository.dart';
+export 'post/post_repository.dart';
+export 'post/base_post_repository.dart';
