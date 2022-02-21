e2cbf4780732d50185100941e671b70a162bf08c
jasonbeaver <jasonbeaverw99@gmail.com>
2022-02-21 15:00:13 -0500

    Finished adding cloud functions

--- functions/index.js
+++ functions/index.js
@@ -30,5 +30,60 @@ exports.onFollowUser = functions.firestore.document(
             userRef.update({following: 1});
         }
 
-        //TODO: Add post logic to the functions. 
+        const followedUserPostRef = admin
+        .firestore
+        .collection('posts')
+        .where('author', '==', followedUserRef);
+
+        const userFeedRef = admin
+        .firestore()
+        .collection('feeds')
+        .doc(followerId)
+        .collection('userFeed');
+        const followedUserPostsSnapshot = await followedUserPostRef.get();
+        
+        followedUserPostsSnapshot.forEach((doc) => {
+            if(doc.exists) {
+                userFeedRef.doc(doc.id).set(doc.data());
+            }
+        });
+    });
+
+    exports.onUnfollowUser = functions
+    .firestore
+    .document('/followers/{userId}/userFollowers/{followerId}')
+    .onDelete(async (_, context) => {
+        const userId = context.params.userId;
+        const followerId = context.params.followerId;
+
+        const followedUserRef = admin.firestore.collection('users').doc(userId);
+        const followedUserDoc = await followedUserRef.get();
+        if (followedUserDoc.get('followers') !== undefined){
+            followedUserRef.update({
+                followers: followedUserDoc.get('followers') - 1,
+            });
+        } else {
+            followedUserRef.update({ followers: 0 });
+        }
+
+        const userRef = admin.firestore().collection('users').doc(followerId);
+        const userDoc = await userRef.get();
+        if(userDoc.get(following) !== undefined) {
+            userRef.update({following: userDoc.get('following') - 1});
+        } else {
+            userRef.update({ following: 0 });
+        }
+
+        const userFeedRef = admin
+        .firestore()
+        .collection('feeds')
+        .doc(followerId)
+        .collection('userFeed')
+        .where('author', '==', followedUserRef);
+        const userPostsSnapshot = await userFeedRef.get();
+        userPostsSnapshot.forEach((doc) => {
+            if(doc.exists){
+                doc.ref.delete();
+            }
+        });
     });
